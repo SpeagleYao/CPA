c85ea0baedfd4d79e76349efe00b8d10685e7c3f
SpeagleYao <1065483675@qq.com>
2022-01-25 00:12:24 +0800

    img aug

--- img_aug.py
+++ img_aug.py
@@ -14,8 +14,7 @@ class data_generator():
         data = np.load(img_dir)
         label = np.load(tar_dir)
 
-        # self.p = Augmentor.DataPipeline(np.stack((data_in, data_out), axis=1))
-        self.p = Augmentor.DataPipeline(data)
+        self.p = Augmentor.DataPipeline(data, label.tolist())
 
         if train:
             self.p.rotate(probability=1.0, max_left_rotation=25, max_right_rotation=25)
@@ -23,8 +22,8 @@ class data_generator():
             self.p.flip_top_bottom(probability=0.5)
             # self.p.skew(probability=0.25)
             # self.p.random_distortion(probability=0.25, grid_height=16, grid_width=16, magnitude=1)
-            # self.p.crop_random(probability=0.5, percentage_area=0.9)
-            self.p.resize(probability=1, width=224, height=224)
+            self.p.crop_random(probability=0.5, percentage_area=0.9)
+            self.p.resize(probability=1, width=512, height=512)
         else:
             seed = 1105
 
@@ -36,21 +35,27 @@ class data_generator():
         self.g = self.p.generator(batch_size)
 
     def gen(self):
-        img_tot = np.array(next(self.g))
-        img_aug = np.expand_dims(img_tot[:,0,:,:], axis=1)/255
-        gt_aug = np.expand_dims(img_tot[:,1,:,:], axis=1)/255
-        img_aug = torch.from_numpy(img_aug).float().detach().requires_grad_(True)
-        gt_aug = torch.from_numpy(gt_aug).float().detach().requires_grad_(True)
+        img_aug, y = next(self.g)
+        img_aug = torch.as_tensor(img_aug).float().detach().requires_grad_(True)
+        y = torch.as_tensor(np.array(y)).float().detach().requires_grad_(True)
+        # print(img_aug.shape, len(y))
+        # img_tot = np.array(next(self.g))
+        # img_aug = np.expand_dims(img_tot[:,0,:,:], axis=1)/255
+        # gt_aug = np.expand_dims(img_tot[:,1,:,:], axis=1)/255
+        # img_aug = torch.from_numpy(img_aug).float().detach().requires_grad_(True)
+        # gt_aug = torch.from_numpy(gt_aug).float().detach().requires_grad_(True)
 
-        return img_aug, gt_aug
+        return img_aug, y
 
 if __name__=='__main__':
-    g = data_generator()
+    img_dir = './data/img_train.npy'
+    tar_dir = './data/tar_train.npy'
+    g = data_generator(img_dir, tar_dir, batch_size=4, train=True)
     img, tar = g.gen()
     print(img.shape, tar.shape) # 256, 1, 128, 128
     print(img[0].max(), img[0].min(), img[0].mean(), img[0].std())
-    ind = 0
-    a = np.hstack((img.detach().numpy()[ind][0]*255, tar.detach().numpy()[ind][0]*255))
-    cv2.imwrite('testimage.png', a)
+    # ind = 0
+    # a = np.hstack((img.detach().numpy()[ind][0]*255, tar.detach().numpy()[ind][0]*255))
+    # cv2.imwrite('testimage.png', a)
     # cv2.imshow('data', img.detach().numpy()[ind][0]*255)
     # cv2.waitKey(0)
