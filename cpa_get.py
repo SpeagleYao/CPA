d145659fdf4b64623362bba7e8227f702c3c4a7d
SpeagleYao <1065483675@qq.com>
2022-01-24 15:22:45 +0800

    label

--- cpa_get.py
+++ cpa_get.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-f2 = open("./CPA_1_1.txt","r")
+file_num = '3_9'
+
+f2 = open("./data/CPA/CPA_" + file_num + ".txt","r")
 lines = f2.readlines()
 a = []
 for line in lines:
     a.append(float(line))
 a = np.array(a)
-# print(a.shape)
-np.save('Seg_label.npy', a)
-# print(a[97])
-# 两个89
+print(a.shape)
+np.save("./data/CPA/label_" + file_num + ".npy", a)
