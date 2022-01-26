9069d7b98cdc6109514af5c8a13b8439585fb470
SpeagleYao <58623495+SpeagleYao@users.noreply.github.com>
2022-01-26 21:45:06 +0800

    Update img_aug.py

--- img_aug.py
+++ img_aug.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
 import Augmentor
-import os
 import shutil
 import random
 import torch
 from torchvision.transforms import transforms
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 
 class data_generator():
     def __init__(self, img_dir, tar_dir, batch_size = 128, train=False, seed=None):
         super(data_generator, self).__init__()
 
         data = np.expand_dims(np.load(img_dir), axis=1)
-        label = np.load(tar_dir)
+        label = np.expand_dims(np.load(tar_dir), axis=1)
         # print(data.shape, label.shape)
 
         self.p = Augmentor.DataPipeline(data, label.tolist())
@@ -26,9 +28,11 @@ class data_generator():
             # self.p.random_distortion(probability=0.25, grid_height=16, grid_width=16, magnitude=1)
             self.p.crop_random(probability=0.5, percentage_area=0.9)
             # self.p.resize(probability=1, width=512, height=512)
-            self.p.resize(probability=1, width=224, height=224)
+            # self.p.resize(probability=1, width=224, height=224)
         else:
             seed = 1105
+
+        self.p.resize(probability=1, width=224, height=224)
 
         if seed:
             np.random.seed(seed)
@@ -53,8 +57,10 @@ class data_generator():
 if __name__=='__main__':
     img_dir = './data/img_train.npy'
     tar_dir = './data/tar_train.npy'
-    g = data_generator(img_dir, tar_dir, batch_size=4, train=True)
+    g = data_generator(img_dir, tar_dir, batch_size=128, train=True)
     img, tar = g.gen()
+    img = img.cuda()
+    tar = tar.cuda()
     print(img.shape, tar.shape) # bs, 1, 512, 512
     print(img[0].max(), img[0].min(), img[0].mean(), img[0].std())
     # ind = 0
